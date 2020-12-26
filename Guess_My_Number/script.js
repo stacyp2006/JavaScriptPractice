4d3f1e36918ca25ffed07ce5b14a2308c043ff77
Stacy Potten <stacylynnaz@gmail.com>
2020-12-26 11:30:44 -0700

    Add highscore functionality

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -16,6 +16,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,6 +32,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //When guess is too high
   } else if (guess > secretNumber) {
