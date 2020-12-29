6d17c4f60e5636057dbda5856cab733a794d303a
Stacy Potten <stacylynnaz@gmail.com>
2020-12-29 09:29:06 -0700

    Display modal and overlay on click

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -7,4 +7,8 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
