35fb750c0772274822e5cf6aa31a2d576a6a77e8
Stacy Potten <stacylynnaz@gmail.com>
2020-12-30 12:20:04 -0700

    Switch players when a 1 is rolled

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -30,8 +32,11 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     //switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
