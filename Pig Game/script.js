d2fcb5700374f20c64eb44f83e468c9f70414ebb
Stacy Potten <stacylynnaz@gmail.com>
2020-12-30 12:09:53 -0700

    Update and display score

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -7,20 +7,31 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
   // 1. generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   // 2. Display the dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //switch to next player
+  }
 });
