bb35f59ad5d7b351f18c7a2594767edc07465b68
Stacy Potten <stacylynnaz@gmail.com>
2020-12-30 13:39:46 -0700

    Hold and display total scores

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -21,6 +21,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
   // 1. generating a random dice roll
@@ -39,10 +47,18 @@ btnRoll.addEventListener('click', function () {
     ).textContent = currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active players score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. check if player's score is >= 100, then finish game
+
+  // Or switch player
+  switchPlayer();
 });
