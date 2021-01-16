df804dc7282c95ba127d676c002aa6739da30af1
Stacy Potten <stacylynnaz@gmail.com>
2021-01-15 20:35:49 -0700

    Add printAge function

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -2,10 +2,19 @@
 
 //defined in the global scope
 function calcAge(birthYear) {
+  //scoped to fxn
   const age = 2037 - birthYear; 
-  console.log(firstName);
+  
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`
+    console.log(output);
+  }
+  printAge();
   return age; 
 } 
 
+//scoped globally
 const firstName = 'Jonas';
+
+
 calcAge(1991);
