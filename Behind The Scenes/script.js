8d278135e395a11efa74b41f993240383e2b6ada
Stacy Potten <stacylynnaz@gmail.com>
2021-01-15 20:43:04 -0700

    Add add function

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -10,13 +10,20 @@ function calcAge(birthYear) {
     console.log(output);
 
     if(birthYear >= 1981 && birthYear <= 1996) {
-      //var is leaky and not scoped to the block 
+      //var is leaky and not scoped to the block, but to the fxn
       var millenial = true; 
       //scoped to the block -- let/const
       const str = `Oh, and you're a millenial, ${firstName}`
       console.log(str);
+
+      //scoped to the block -- only true in strict mode
+      function add(a, b) {
+        return a + b;
+      }
     }
     console.log(millenial);
+    // console.log(add(2, 3))
+
   }
   printAge();
   return age; 
