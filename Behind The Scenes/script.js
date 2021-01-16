c04cc22fb768d092c4b1f97eee9b8321e637e743
Stacy Potten <stacylynnaz@gmail.com>
2021-01-15 20:38:12 -0700

    Add if block

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -8,6 +8,13 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName}, you are ${age}, born in ${birthYear}`
     console.log(output);
+
+    if(birthYear >= 1981 && birthYear <= 1996) {
+      //scoped to the block -- let/const
+      const str = `Oh, and you're a millenial, ${firstName}`
+      console.log(str);
+    }
+    
   }
   printAge();
   return age; 
