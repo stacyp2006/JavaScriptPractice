56b3c648bb82ba961dfa9b4bf8959671058a6805
Stacy Potten <stacylynnaz@gmail.com>
2021-01-16 22:35:26 -0700

    Exploring hoisting

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -39,3 +39,27 @@ const firstName = 'Jonas';
 
 
 calcAge(1991);
+
+//Variable hoisting
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas'
+let job = 'teacher'
+const year = 1991;
+
+//hoisting w/fxns
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b; 
+}
+
+const addArrow = (a, b) => a + b;
