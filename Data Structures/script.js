1cccc5649eb83346c4727a2d670a1c85786c3c8a
Stacy Potten <stacylynnaz@gmail.com>
2021-01-24 16:41:00 -0700

    Writing methods in objects with ES6 enhancements

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -23,28 +23,24 @@ const restaurant = {
   //ES6 enhanced object literals
   openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  //ES6 enhanced obj literals: no need for function keyword
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
