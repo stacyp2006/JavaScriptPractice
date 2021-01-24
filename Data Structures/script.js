949c0137688c4d1ad0e38ef9e6abbe8d71b297d3
Stacy Potten <stacylynnaz@gmail.com>
2021-01-23 20:20:46 -0700

    Explore short circuiting with or operator

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -126,29 +126,29 @@ const restaurant = {
 //The Spread Operator
 //Acts similar to destructuring. Difference: takes all elements, doesn't create new variables. Can only use it in places where we would otherwise write values separated by commas
 
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
 
-const newGoodArr = [1, 2, ...arr]; //expands the array into all of its individual elements
-console.log(newGoodArr);
+// const newGoodArr = [1, 2, ...arr]; //expands the array into all of its individual elements
+// console.log(newGoodArr);
 
-console.log(...newGoodArr);
+// console.log(...newGoodArr);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+// console.log(newMenu);
 
 //Copy array --  creates shallow copy
-const mainMenuCopy = [...restaurant.mainMenu];
+// const mainMenuCopy = [...restaurant.mainMenu];
 
 //Join 2 or more arrays
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(menu);
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
 
 //Iterables are arrays, strings, maps and sets but not objects
-const str = 'Jonas';
-const letters = [...str, ' ', 'S.'];
-console.log(letters);
+// const str = 'Jonas';
+// const letters = [...str, ' ', 'S.'];
+// console.log(letters);
 
 //Real World Example
 // const ingredients = [
@@ -161,44 +161,53 @@ console.log(letters);
 // restaurant.orderPasta(...ingredients);
 
 //Objects
-const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
-console.log(newRestaurant);
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
 
 // The Rest Pattern
-const arr1 = [1, 2, ...[3, 4]]; // Spread because on right side of =
+// const arr1 = [1, 2, ...[3, 4]]; // Spread because on right side of =
 
-const [a, b, ...others] = [1, 2, 3, 4, 5]; //Rest because on left side of =
-console.log(a, b, others);
+// const [a, b, ...others] = [1, 2, 3, 4, 5]; //Rest because on left side of =
+// console.log(a, b, others);
 
 // rest pattern must be last element
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
 //Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
 // Functions
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
 
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 6, 1, 4);
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 6, 1, 4);
 
-const x = [23, 5, 7];
-add(...x);
+// const x = [23, 5, 7];
+// add(...x);
 
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms');
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
+
+//use, return any data type and can short circuit
+//returns the first value as long as it is truthy, doesn't even eval the other value
+console.log(3 || 'Jonas'); //3
+console.log('' || 'Jonas'); //'Jonas'
+console.log(true || 0); //true
+console.log(undefined || null); //null, also falsy but prints anyway
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); //Hello, b/c it is the first truthy value, short circuits the ones after
