f1d44bedd80fdfdc926ca0a6cc77ab71737e04d6
Stacy Potten <stacylynnaz@gmail.com>
2021-01-25 16:20:30 -0700

    Add function to calculate average odds for task 2

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -393,11 +393,20 @@ const game = {
 //to get item and index
 
 const players = [...game.scored];
-console.log(players);
 for (let i = 0; i < players.length; i++) {
   console.log(`Goal ${[i + 1]}: ${players[i]}`);
 }
 // 2. Use a loop to calculate the average odd and log it to the console(We already studied how to calculate averages, you can go check if you don't remember)
+const values = Object.values(game.odds);
+let sum = 0;
+const average = function () {
+  for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+  }
+  return sum / values.length;
+};
+
+console.log(average());
 // 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
 // Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
 // Odd of victory Borrussia Dortmund: 6.5
