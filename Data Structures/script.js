2c8dc7396bde9ff952fe491893360e6b5b0395d6
Stacy Potten <stacylynnaz@gmail.com>
2021-01-24 20:49:54 -0700

    Explore with Object.entries

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -63,6 +63,13 @@ console.log(openStr);
 const values = Object.values(openingHours);
 console.log(values);
 
+//Entries (names + values)
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
 // if (restaurant.openingHours && restaurant.openingHours.mon)
 //   console.log(restaurant.openingHours.mon.open);
 
