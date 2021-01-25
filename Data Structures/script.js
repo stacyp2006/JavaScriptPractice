87454cc162b51d145f773e676cd2daf4800c8005
Stacy Potten <stacylynnaz@gmail.com>
2021-01-25 16:28:06 -0700

    Add alt solutions for tasks 1 and 2

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -392,11 +392,19 @@ const game = {
 // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 //to get item and index
 
+//My solution
 const players = [...game.scored];
 for (let i = 0; i < players.length; i++) {
   console.log(`Goal ${[i + 1]}: ${players[i]}`);
 }
+
+//Jonas solution
+for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${player}`);
+
 // 2. Use a loop to calculate the average odd and log it to the console(We already studied how to calculate averages, you can go check if you don't remember)
+
+//My Solution
 const values = Object.values(game.odds);
 let sum = 0;
 const average = function () {
@@ -405,12 +413,21 @@ const average = function () {
   }
   return sum / values.length;
 };
-
 console.log(average());
+
+//Jonas solution
+const odds = Object.values(game.odds);
+let average1 = 0;
+for (const odd of odds) average1 += odd;
+average1 /= odds.length;
+console.log(average1);
+
 // 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
-// Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
 // Odd of victory Borrussia Dortmund: 6.5
 // Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+
 // 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
 //      {
 //        Gnarby: 1,
