d70e8d16ea0a094901443a99fa2919c33412b44a
Stacy Potten <stacylynnaz@gmail.com>
2021-01-28 20:55:21 -0700

    Mini quiz using Map

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -62,6 +62,17 @@ console.log(question);
 console.log(Object.entries(openingHours));
 const hoursMap = new Map(Object.entries(openingHours));
 console.log(hoursMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
 //MAPS: fundamentals
 // const rest = new Map();
 // rest.set('name', 'Classico Italiano');
