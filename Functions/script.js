a4e92fde1544c8d11eea45ba8824e951af276321
Stacy Potten <stacylynnaz@gmail.com>
2021-02-03 14:07:36 -0700

    Add checkIn method to practice passing arguments

--- Functions/script.js
+++ Functions/script.js
@@ -1,27 +1,48 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  //default params are ES6
-  //ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //default params are ES6
+//   //ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 200); //how to skip a default param
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 247869584738,
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
-createBooking('LH123', undefined, 200); //how to skip a default param
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH234';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 247869584738) {
+    alert('Check In');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
