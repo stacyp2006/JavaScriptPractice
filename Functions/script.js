afddd0a255dac85cfc4d5ce04b53028802176c79
Stacy Potten <stacylynnaz@gmail.com>
2021-02-02 20:48:33 -0700

    Add default parameters

--- Functions/script.js
+++ Functions/script.js
@@ -2,7 +2,15 @@
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //default params are ES6
+  //ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
   const booking = {
     flightNum,
     numPassengers,
@@ -13,3 +21,7 @@ const createBooking = function (flightNum, numPassengers, price) {
 };
 
 createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 200); //how to skip a default param
