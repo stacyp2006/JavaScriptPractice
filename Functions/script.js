504f949ce7ee1d2caf517d64b5ad89691af365f9
Stacy Potten <stacylynnaz@gmail.com>
2021-02-06 19:00:42 -0700

    Add buy planes method and event listener

--- Functions/script.js
+++ Functions/script.js
@@ -155,3 +155,13 @@ bookEW(23, 'Steven Williams');
 
 const bookEW23 = book.bind(eurowings, 23);
 bookEW23('Jonas Schmedtmann');
+
+//With event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
