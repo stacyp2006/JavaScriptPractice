455f2db9c921673ed93630112e2f44ea2bdca7d9
Stacy Potten <stacylynnaz@gmail.com>
2021-02-06 20:40:33 -0700

    Connect button to method

--- Functions/script.js
+++ Functions/script.js
@@ -215,10 +215,16 @@ const poll = {
       )
     );
     console.log(answer);
+    //Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
   },
 };
 
 // poll.registerNewAnswer();
-// document
-//   .querySelector('.poll')
-//   .addEventListener('click', poll.registerNewAnswer);
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
