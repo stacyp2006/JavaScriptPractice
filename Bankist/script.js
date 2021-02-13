b132a69b981ef84a5edbbc44516c42b1a65b71da
Stacy Potten <stacylynnaz@gmail.com>
2021-02-12 21:04:46 -0700

    Explore reverse and concat methods

--- Bankist/script.js
+++ Bankist/script.js
@@ -88,3 +88,12 @@ console.log(arr.splice(2));
 // console.log(arr.splice(2));
 arr.splice(-1);
 console.log(arr);
+
+//reverse method -- mutates the original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+//concat method
+const letters = arr.concat(arr2);
+console.log(letters); //same as [...arr, ...arr2]
