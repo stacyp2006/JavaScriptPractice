3763edabfb26212c714007edfc5211e9a08d8ad8
Stacy Potten <stacylynnaz@gmail.com>
2021-02-12 20:55:32 -0700

    Explore slice method

--- Bankist/script.js
+++ Bankist/script.js
@@ -74,3 +74,14 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//slice method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice()); //shallow copy
+
+//splice method
+console.log(arr.splice(2));
